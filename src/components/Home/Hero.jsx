c03fe1c8410534939f76b659583830a06386052c
Haarshdubeyy <harshdubey1602@gmail.com>
2024-10-08 18:46:09 +0530

    add some beer css

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -7,8 +7,7 @@ const Hero = () => {
     <div className="hero bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 h-screen flex justify-center items-center">
       <Link
         to="/create-note"
-        className="beer-button text-white text-xl font-bold px-8 py-4 rounded-lg shadow-lg bg-black bg-opacity-50 hover:bg-opacity-70 transition duration-300 ease-in-out"
-      >
+        className="beer-button text-white text-xl font-bold px-8 py-4 rounded-lg shadow-lg bg-black bg-opacity-50 hover:bg-opacity-70 transition duration-300 ease-in-out">
         Create New Note
       </Link>
     </div>
